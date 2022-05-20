b0a27cd4cb136b16ebbe0110b3c62c474d4e8b46
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-20 09:58:52 +0200

    Adding auto bind decorator

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,51 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autoBind(_, _2, descriptor) {
+    console.log(descriptor.value);
+    var originalMethod = descriptor.value;
+    var adjustedDescriptor = {
+        configurable: true,
+        get: function () {
+            var fn = originalMethod.bind(this);
+            return fn;
+        }
+    };
+    console.log(adjustedDescriptor);
+    return adjustedDescriptor;
+}
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
-        this.attachTemplateForm();
+        this.formElement = this.importTemplateContent().firstElementChild;
+        this.attachTemplateForm(this.formElement);
+        this.titleInput = this.formElement.querySelector('#title');
+        this.descriptionInput = this.formElement.querySelector('#description');
+        this.peopleInput = this.formElement.querySelector('#people');
+        this.configureForm();
     }
     ProjectInput.prototype.importTemplateContent = function () {
         var importedNode = document.importNode(this.templateElement, true);
         return importedNode.content;
     };
-    ProjectInput.prototype.attachTemplateForm = function () {
-        var templateForm = this.importTemplateContent().firstElementChild;
-        this.hostElement.insertAdjacentElement('afterbegin', templateForm);
+    ProjectInput.prototype.attachTemplateForm = function (form) {
+        this.hostElement.insertAdjacentElement('afterbegin', form);
     };
+    ProjectInput.prototype.configureForm = function () {
+        this.formElement.addEventListener("submit", this.sumbitHandler);
+    };
+    ProjectInput.prototype.sumbitHandler = function (event) {
+        event.preventDefault();
+        console.log(this.titleInput.value);
+    };
+    __decorate([
+        autoBind
+    ], ProjectInput.prototype, "sumbitHandler", null);
     return ProjectInput;
 }());
-var iiii = new ProjectInput();
+var projectInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,40 @@
+
+
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor){
+    console.log(descriptor.value);
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(){
+            const fn = originalMethod.bind(this);
+            return fn;
+        }
+    }
+
+    console.log(adjustedDescriptor)
+    return adjustedDescriptor;
+}
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
 
+    formElement: HTMLFormElement;
+
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput: HTMLInputElement;
+
     constructor(){
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        this.formElement = this.importTemplateContent().firstElementChild as HTMLFormElement;
+        this.attachTemplateForm(this.formElement);
 
-        this.attachTemplateForm();
+        this.titleInput = this.formElement.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInput = this.formElement.querySelector('#description')! as HTMLInputElement;
+        this.peopleInput = this.formElement.querySelector('#people')! as HTMLInputElement;
+
+        this.configureForm()
     }
 
     private importTemplateContent(){
@@ -14,10 +42,19 @@ class ProjectInput {
         return importedNode.content;
     }
 
-    private attachTemplateForm(){
-        const templateForm = this.importTemplateContent().firstElementChild as HTMLFormElement;
-        this.hostElement.insertAdjacentElement('afterbegin', templateForm);
+    private attachTemplateForm(form: HTMLFormElement){
+        this.hostElement.insertAdjacentElement('afterbegin', form);
+    }
+
+    private configureForm(){
+        this.formElement.addEventListener("submit", this.sumbitHandler);
+    }
+
+    @autoBind
+    private sumbitHandler(event: Event){
+        event.preventDefault();
+        console.log(this.titleInput.value);
     }
 }
 
-const iiii = new ProjectInput();
+const projectInput = new ProjectInput();
