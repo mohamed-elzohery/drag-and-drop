0cb3beff65e9e1ca0d237e17122dd9c7ad17394f
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-20 07:44:44 +0200

    Creating project input class

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("svkmsfklv");
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        this.attachTemplateForm();
+    }
+    ProjectInput.prototype.importTemplateContent = function () {
+        var importedNode = document.importNode(this.templateElement, true);
+        return importedNode.content;
+    };
+    ProjectInput.prototype.attachTemplateForm = function () {
+        var templateForm = this.importTemplateContent().firstElementChild;
+        this.hostElement.insertAdjacentElement('afterbegin', templateForm);
+    };
+    return ProjectInput;
+}());
+var iiii = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log("svkmsfklv")
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+
+    constructor(){
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        this.attachTemplateForm();
+    }
+
+    private importTemplateContent(){
+        const importedNode = document.importNode(this.templateElement, true);
+        return importedNode.content;
+    }
+
+    private attachTemplateForm(){
+        const templateForm = this.importTemplateContent().firstElementChild as HTMLFormElement;
+        this.hostElement.insertAdjacentElement('afterbegin', templateForm);
+    }
+}
+
+const iiii = new ProjectInput();
